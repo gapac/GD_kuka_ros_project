140026cdf3c0e3209255818768b5199c3f6139c4
David Hozic <davidhozic@gmail.com>
2024-03-15 22:13:08 +0100

    renamed

--- src/projekt_vr/scripts/main.py
+++ src/projekt_vr/scripts/main.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 """
 Main node file for Kuka mobile platform and arm.
 """
-from platform import *
+
+from robot import *
 
 platform = Platform("/project_kuka/platform", MobileController(), LaserScanner(zero_angle_index=270))
 platform.track_closest_object(min_distance=0.05, max_distance=2, target_x=0.5)
-
